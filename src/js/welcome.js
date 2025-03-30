4a23195c28d19df7170923bf2a1e2ac5fa275495
susahbangun <suhud4rt@gmail.com>
2025-03-31 00:59:15 +0700

    Update welcome.js

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -24,7 +24,7 @@ export const welcome = () => {
         const params = getQueryParameter('to');
 
         if (params) {
-            weddingToElement.innerHTML = `Kepada Yth Bapak/Ibu/Saudara/i<br><span>${params}</span>`;
+            weddingToElement.innerHTML = `Kepada Yth Bapak/Ibu/Saudara/(i)<br><span>${params}</span>`;
             name.value = params;
         } else {
             weddingToElement.innerHTML = `Kepada Yth Bapak/Ibu/Saudara/(i)<br><span>Teman-teman semua</span>`;
