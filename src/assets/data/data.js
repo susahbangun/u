af156e5cb051d758bdfbbaa5e9eca9c83e2f71e9
susahbangun <suhud4rt@gmail.com>
2025-03-30 20:37:33 +0700

    Update data.js

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -99,7 +99,7 @@ export const data = {
         },
     ],
 
-    audio: './src/assets/music/v.mp4',
+    audio: './src/assets/audio/x.mp4',
 
     api: 'https://script.google.com/macros/s/AKfycbwEc7FF6KwTdoLSxpbmddG24O7lK6hlSBH4m6nwdKRwGubEKeGazJm2fSkhFyYN_R9Gqw/exec',
 
