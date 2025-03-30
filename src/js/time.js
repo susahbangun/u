366672c4f7c5e1027961b7abfda3c3f075e610c6
susahbangun <suhud4rt@gmail.com>
2025-03-31 00:56:24 +0700

    Add files via upload

---
+++ src/js/time.js
@@ -0,0 +1,22 @@
+import { data } from "../assets/data/data.js";
+
+export const time = () => {
+    const timeContainer = document.querySelector('.time');
+    const [marriageDiv, receptionDiv] = timeContainer.querySelectorAll('div div');
+    const mapLink = timeContainer.querySelector('a');
+    const addressParagraph = timeContainer.querySelector('a + p');
+
+    const createTimeListItem = (title, details) => (
+        `<h3>${title}</h3>
+         <p>${details.day}, ${details.date} ${details.month} ${details.year} <br> 
+         Pukul ${details.hours.start} WIB sd ${details.hours.finish}</p>`
+    );
+
+    marriageDiv.innerHTML = createTimeListItem('Mengantar Mempelai Pria', data.time.marriage);
+
+    
+    
+    receptionDiv.innerHTML = createTimeListItem('Resepsi', data.time.reception);
+
+    
+};
